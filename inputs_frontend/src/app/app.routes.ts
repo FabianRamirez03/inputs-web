c15868b799d5c0aa4d947fd4fbfa969b1c588213
fabian <ramirezfabian003@gmail.com>
2024-08-24 09:07:44 -0600

    Hover over al drop down menu. El contenido ya no sale detras del header

--- inputs_frontend/src/app/app.routes.ts
+++ inputs_frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from "./sections/home/landing-page/landing-page.component";
+import { ServicesHomeComponent } from './sections/services/services-home/services-home.component';
 
 export  const routes: Routes = [
     { path: '', component: LandingPageComponent },
-    { path: 'hola', component: LandingPageComponent },
+    { path: 'servicios', component: ServicesHomeComponent },
   ];
---
+++ inputs_frontend/src/app/sections/services/services-home/services-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './services-home.component.html',
+  styleUrl: './services-home.component.scss'
+})
+export class ServicesHomeComponent {
+
+}
